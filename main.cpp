da781f791a77187220d65735ac5395b6be83871c
Christian Riess <christian.riess@fau.de>
2010-05-05 09:19:05 +0000

    some comments on the example

--- main.cpp
+++ main.cpp
@@ -72,25 +72,50 @@ int main(int argc, char** argv) {
 	if (image.empty())
 		return 2;
 
-/*	multi_img::Pixel a = image(0, 0);
-	cv::Mat_<multi_img::Value> b = image.Matrix(a);
-	b = image.Matrix(image(1, 1));
-	b.setTo(255.);
-	image.setPixel(1, 1, b);
-	image.setPixel(cv::Point(2, 2), multi_img::Pixel(31, 255.));
+/* Example for the multi_img API
+	// get pixel at position 0, 0
+	multi_img::Pixel a = image(0, 0);
 
+	// convert pixel to cv::Mat
+	cv::Mat_<multi_img::Value> b = image.Matrix(a);
+
+	// alternatively: construct the matrix in one line
+	b = image.Matrix(image(1, 1));
+
+	// all values -> 255
+	b.setTo(255.);
+
+	// copy these values back in the multispectral image
+	image.setPixel(1, 1, b);
+
+	// quicker to type: do the same stuff as above by creating a pixel of the
+	// required dimension, where all entries are initialized with 255
+	image.setPixel(cv::Point(2, 2), multi_img::Pixel(image.size(), 255.));
+
+	// work on a segment of the image:
+	// this always requires a matrix containing a mask with the required
+	// pixels. multi_img::Mask is internally a cv::Mat_<uchar>
 	multi_img::Mask m(image.height, image.width, (uchar)0);
+
+	// mark some pixels, in this example with a circle
 	cv::circle(m, cv::Point(63, 63), 50, 255, 10);
+
+	// now there are two possibilities to work with this segment: either call
+	// getSegment, which returns a std::vector of pixel const pointers to the
+	// selected image pixels. This is highly efficient, but const.
+	// Second, it is possible to retrieve a copy of the pixels. These can be
+	// arbitrarily manipulated.
 	vector<multi_img::Pixel> v = image.getSegmentCopy(m);
+	// thresholding example for the 0'th dimension (in this case, at 400nm)
 	for (int i = 0; i < v.size(); ++i) {
 		if (v[i][0] < 25.)
 			v[i].assign(image.size(), image.minval);
 		else
 			v[i].assign(image.size(), image.maxval);
 	}
+	// copy the segment back in the image.
 	image.setSegment(v, m);
 */
-
 	//TODO: input filenames with path elements
 	string base = options.outputdir + "/" + options.inputfile;
 
