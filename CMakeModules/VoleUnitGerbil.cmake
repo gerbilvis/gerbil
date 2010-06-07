776a9e81e87d244a7a4a45f58d2342284cd515d0
Johannes Jordan <git@lanrules.de>
2010-06-07 19:24:54 +0000

    first gerbil+vole that does not work

---
+++ CMakeModules/VoleUnitGerbil.cmake
@@ -0,0 +1,7 @@
+set(vole_current_component "gerbil")
+set(vole_component_symbol "GERBIL")
+set(vole_component_long_name "Modules for Multispectral Image Processing")
+
+set(VOLE_CURRENT_UNIT "gerbil")
+set(VOLE_UNITS ${VOLE_UNITS} ${VOLE_CURRENT_UNIT})
+
