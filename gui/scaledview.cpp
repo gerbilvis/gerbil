7193a8a8720df33909bc222e7b3df112a3602d3c
Johannes Jordan <git@lanrules.de>
2010-09-20 16:55:34 +0000

    copyright messages added, gplv3

--- gui/scaledview.cpp
+++ gui/scaledview.cpp
@@ -1,3 +1,11 @@
+/*	
+	Copyright(c) 2010 Johannes Jordan <johannes.jordan@cs.fau.de>.
+
+	This file may be licensed under the terms of of the GNU General Public
+	License, version 3, as published by the Free Software Foundation. You can
+	find it here: http://www.gnu.org/licenses/gpl.html
+*/
+
 #include "scaledview.h"
 
 #include <stopwatch.h>
