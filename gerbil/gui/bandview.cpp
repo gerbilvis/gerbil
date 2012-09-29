f1ba198f2e04f413ff6ab0d7f06913cf75689cd9
Petr Koupý <petr.koupy@gmail.com>
2012-09-29 04:45:10 +0000

    Added copyright notices to the files with significant changes.

--- gerbil/gui/bandview.cpp
+++ gerbil/gui/bandview.cpp
@@ -1,5 +1,6 @@
 /*	
 	Copyright(c) 2010 Johannes Jordan <johannes.jordan@cs.fau.de>.
+	Copyright(c) 2012 Petr Koupy <petr.koupy@gmail.com>
 
 	This file may be licensed under the terms of of the GNU General Public
 	License, version 3, as published by the Free Software Foundation. You can
--- gerbil/gui/multi_img_viewer.h
+++ gerbil/gui/multi_img_viewer.h
@@ -1,3 +1,12 @@
+/*
+	Copyright(c) 2012 Johannes Jordan <johannes.jordan@cs.fau.de>.
+	Copyright(c) 2012 Petr Koupy <petr.koupy@gmail.com>
+
+	This file may be licensed under the terms of of the GNU General Public
+	License, version 3, as published by the Free Software Foundation. You can
+	find it here: http://www.gnu.org/licenses/gpl.html
+*/
+
 #ifndef MULTI_IMG_VIEWER_H
 #define MULTI_IMG_VIEWER_H
 
