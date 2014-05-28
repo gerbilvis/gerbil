4eddb064976acebda402f71d7b016d930ff033ad
Johannes Jordan <git@lanrules.de>
2014-05-28 14:40:18 +0000

    remove unused abort()

--- shell/command.h
+++ shell/command.h
@@ -25,7 +25,6 @@ public:
 	virtual const std::string& getContributorMail() const { return contributor_mail; }
 	virtual Config& getConfig() { return abstract_config; }
 	virtual int execute() = 0;
-	virtual void abort() {}
 	virtual std::map<std::string, boost::any> execute(std::map<std::string, boost::any> &input, ProgressObserver *progress = NULL) { /*dummy*/ assert(false); std::map<std::string, boost::any> a; return a; }
 	virtual void printShortHelp() const = 0;
 	virtual void printHelp() const = 0;
