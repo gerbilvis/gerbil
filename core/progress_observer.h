4e82ac5d6e96813e120fd0bb956ab58f9f51d9f1
Johannes Jordan <git@lanrules.de>
2014-05-31 21:56:37 +0000

    file missing from last commit

--- core/progress_observer.h
+++ core/progress_observer.h
@@ -8,8 +8,15 @@ class ProgressObserver
 public:
 	virtual ~ProgressObserver() {}
 
-	/// if false: cancel job
-	virtual bool update(int percent) = 0;
+	/** provide an update and check if you should continue
+	 * @param report the progress to report, between 0 and 1 (100% done)
+	 * @param incremental the report is an increment (false: absolute progress)
+	 * @return if false, cancel the computation ASAP
+	 * @note This is not thread-safe. But progress output is not
+	 *  mission-critical. So it is o.k. to call from different threads.
+	 *  The abort mechanism is thread-safe though.
+	 */
+	virtual bool update(float report, bool incremental = false) = 0;
 };
 
 /* For smaller tasks in a bigger calculation */
@@ -17,14 +24,26 @@ class ChainedProgressObserver : public ProgressObserver
 {
 public:
 	ChainedProgressObserver(ProgressObserver *target, float fraction)
-		: target(target), fraction(fraction) {}
+		: target(target), fraction(fraction), reported(0.f) {}
 
-	bool update(int percent) {
-		return (!target) || target->update(percent * fraction);
+	bool update(float report, bool incremental = false) {
+		if (!target)
+			return true;
+		/* Note: we need to do our own book-keeping to translate to correct
+		 * incremental updates */
+		if (incremental) {
+			reported += report;
+			return target->update(report * fraction, incremental);
+		} else {
+			float gain = report - reported;
+			reported = report;
+			return target->update(gain * fraction, true);
+		}
 	}
 
 	ProgressObserver *target;
 	float fraction;
+	float reported;
 };
 
 }
