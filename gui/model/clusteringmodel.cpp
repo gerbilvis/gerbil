gerbilvis/gerbil

commit 79f413e7175bb25342b9c59eeaf0e1eaf572e720

fix compilation without meanshift module

gui/model/clusteringmodel.cpp
@@ -1,5 +1,7 @@
 #include "clusteringmodel.h"
 
+#ifdef WITH_SEG_MEANSHIFT
+
 #include <commandrunner.h>
 #include <meanshift_shell.h>
 // vole modules
@@ -316,3 +318,5 @@ void ClusteringModel::abortCommandRunner()
 			commandRunner, SLOT(deleteLater()));
 	commandRunner = NULL;
 }
+
+#endif

gui/model/clusteringmodel.h
@@ -1,6 +1,8 @@
 #ifndef CLUSTERING_MODEL_H
 #define CLUSTERING_MODEL_H
 
+#ifdef WITH_SEG_MEANSHIFT
+
 #include <gerbil_cplusplus.h>
 #include <shared_data.h>
 
@@ -87,5 +89,7 @@ protected:
 	// IMG and GRAD representation of multi image of current ROI.
 	QMap<representation::t, SharedMultiImgPtr> inputMap;
 };
+
+#endif
 
 #endif // CLUSTERING_MODEL_H

gui/controller/controller.cpp
@@ -104,13 +104,14 @@ Controller::Controller(const QString &filename,
 			dvc, SIGNAL(newIlluminantCurve(QVector<multi_img::Value>)));
 	connect(illumm, SIGNAL(newIlluminantApplied(QVector<multi_img::Value>)),
 			dvc, SIGNAL(newIlluminantApplied(QVector<multi_img::Value>)));
-
+#ifdef WITH_SEG_MEANSHIFT
 	connect(cm, SIGNAL(subscribeRepresentation(QObject*,representation::t)),
 			this, SLOT(subscribeRepresentation(QObject*,representation::t)));
 	connect(cm, SIGNAL(unsubscribeRepresentation(QObject*,representation::t)),
 			this, SLOT(unsubscribeRepresentation(QObject*,representation::t)));
 	connect(im, SIGNAL(imageUpdate(representation::t,SharedMultiImgPtr,bool)),
 			cm, SLOT(processImageUpdate(representation::t,SharedMultiImgPtr,bool)));
+#endif
 	connect(this, SIGNAL(preROISpawn(cv::Rect,cv::Rect,std::vector<cv::Rect>,std::vector<cv::Rect>,bool)),
 			dvc, SLOT(processPreROISpawn(cv::Rect,cv::Rect,std::vector<cv::Rect>,std::vector<cv::Rect>,bool)));
 	connect(this, SIGNAL(postROISpawn(cv::Rect,cv::Rect,std::vector<cv::Rect>,std::vector<cv::Rect>,bool)),

gui/controller/controller.h
@@ -18,7 +18,9 @@ class DistViewController;
 class GraphSegmentationModel;
 class FalseColorModel;
 class LabelingModel;
+#ifdef WITH_SEG_MEANSHIFT
 class ClusteringModel;
+#endif
 class IllumModel;
 class ImageModel;
 class MainWindow;
@@ -55,7 +57,9 @@ public:
 	FalseColorModel* falseColorModel() { return fm; }
 	IllumModel* illumModel() { return illumm; }
 	GraphSegmentationModel* graphSegmentationModel() { return gsm; }
+#ifdef WITH_SEG_MEANSHIFT
 	ClusteringModel* clusteringModel() { return cm; }
+#endif
 
 signals:
 	void currentLabelChanged(int);

commit 1b90b55046e185aa14412447117278899012694d

fix moc'ing with mean shift module
this fixes a regression introduced in
79f413e7175bb25342b9c59eeaf0e1eaf572e720
vole does not propagate module availability defines to qt moc :(

gui/model/clusteringmodel.cpp
@@ -1,13 +1,12 @@
 #include "clusteringmodel.h"
 
-#ifdef WITH_SEG_MEANSHIFT
-
 #include <commandrunner.h>
+#ifdef WITH_SEG_MEANSHIFT
 #include <meanshift_shell.h>
-// vole modules
 #include <meanshift.h>
 #include <meanshift_shell.h>
 #include <meanshift_sp.h>
+#endif
 
 #include <map>
 #include <boost/any.hpp>
@@ -72,6 +71,7 @@ void ClusteringModel::requestSegmentation(const ClusteringRequest &r)
 
 void ClusteringModel::startSegmentation()
 {
+#ifdef WITH_SEG_MEANSHIFT
 	bool good = true;
 	if (!request) {
 		std::cerr << "ClusteringModel::startSegmentation(): "
@@ -198,6 +198,7 @@ void ClusteringModel::startSegmentation()
 		   <<  static_cast<CommandRunner*>(commandRunner) << endl);
 	commandRunner->start();
 	state = State::Executing;
+#endif
 }
 
 
@@ -319,4 +320,3 @@ void ClusteringModel::abortCommandRunner()
 	commandRunner = NULL;
 }
 
-#endif

gui/model/clusteringmodel.h
@@ -1,8 +1,6 @@
 #ifndef CLUSTERING_MODEL_H
 #define CLUSTERING_MODEL_H
 
-#ifdef WITH_SEG_MEANSHIFT
-
 #include <shared_data.h>
 
 #include <model/representation.h>
@@ -88,7 +86,5 @@ protected:
 	// IMG and GRAD representation of multi image of current ROI.
 	QMap<representation::t, SharedMultiImgPtr> inputMap;
 };
-
-#endif
 
 #endif // CLUSTERING_MODEL_H