657cc92556583979743e982d180bb67dd4b50f10
ypnos <git@lanrules.de>
2016-03-25 18:12:18 +0100

    Enable setDefaultAction()
    The drawing bug encountered back then seems to be resolved

--- gui/widgets/ahcombobox.cpp
+++ gui/widgets/ahcombobox.cpp
@@ -18,8 +18,7 @@ void AHComboBox::showPopup()
 	// map to scene coordinates
 	QPoint scenepoint = mapToGlobal(QPoint(0, 0));
 	menu.setActiveAction(actions[currentIndex()]);
-	// would be nice, but has a drawing bug in Qt (text too wide for window)
-	// menu.setDefaultAction(actions[currentIndex()]);
+	menu.setDefaultAction(actions[currentIndex()]);
 	QAction *a = menu.exec(scenepoint, actions[currentIndex()]);
 	if (!a)
 		return;
